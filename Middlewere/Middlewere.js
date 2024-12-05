0d13a052eeb2c14411751292e03c61ff9b63075b
Khaiqol <Khaiqol@gmail.com>
2024-12-05 08:03:24 +0700

    STYLING_PAWC_115

--- Middlewere/Middlewere.js
+++ Middlewere/Middlewere.js
@@ -1,10 +1,14 @@
-// Middleware untuk memeriksa autentikasi
+// Middleware untuk memeriksa apakah pengguna sudah login
 function isAuthenticated(req, res, next) {
+    // Cek apakah ada 'userId' di session pengguna
     if (req.session.userId) {
+        // Kalau ada 'userId', berarti pengguna sudah login, lanjutkan ke langkah berikutnya
         return next();
     } else {
+        // Kalau 'userId' tidak ada, berarti pengguna belum login, arahkan ke halaman login
         res.redirect('/login');
     }
 }
 
+// Ekspor middleware isAuthenticated supaya bisa dipakai di tempat lain
 module.exports = { isAuthenticated };
--- dependency.mjs
+++ dependency.mjs
@@ -1,6 +1,11 @@
+// Mengimpor modul lodash, khususnya fungsi-fungsi utilitasnya
 import _ from "lodash";
 
-const source = "Khaiqal Satrio"
-const target = _.capitalize(source)
+// Mendeklarasikan variabel source yang berisi string "Khaiqal Satrio"
+const source = "Khaiqal Satrio";
 
+// Menggunakan fungsi capitalize dari lodash untuk mengubah huruf pertama menjadi kapital
+const target = _.capitalize(source);
+
+// Menampilkan hasil yang sudah dimodifikasi
 console.info(target);
